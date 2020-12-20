fb40d123027e0b28f91a6e5ded0d731562639be3
Ronic <abdoronic@gmail.com>
2020-12-21 01:28:56 +0200

    Add simple solution visualizer

--- planner/io/visualizer.py
+++ planner/io/visualizer.py
@@ -1,2 +1,11 @@
 def visualize_floor(floor, solver):
-    pass
+    print(solver.StatusName())
+
+    for number, room in enumerate(floor.rooms, 1):
+        (xs, xe, ys, ye) = room.variables
+        bounds = (solver.Value(xs), solver.Value(xe), solver.Value(ys), solver.Value(ye))
+        print(
+            f'Room #{number}:',
+            f'nw{bounds[0], bounds[3]}, ne{bounds[1], bounds[3]}',
+            f'sw{bounds[0], bounds[2]}, se{bounds[1], bounds[2]}'
+        )
