81f06e5becd7a3d75446690ab2769e7e07befaf4
Ronic <abdoronic@gmail.com>
2020-12-28 02:59:11 +0200

    Add adjacentTo constraint

--- planner/constrainers/apartment_constrainer.py
+++ planner/constrainers/apartment_constrainer.py
@@ -1,2 +1,8 @@
+from planner.constrainers.room_constrainer import enforce_rooms_be_adjacent
+
 def constrain_apartment(apartment, floor, model):
-    pass
+    for room in apartment.rooms:
+        for other_room_idx in room.adjacent_to:
+            other_room = apartment.rooms[other_room_idx]
+            enforce_rooms_be_adjacent(room, other_room, model)
+    
