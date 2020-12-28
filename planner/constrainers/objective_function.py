8a77810b355158f6e0ea89f7dbc8b1dea899f885
Ronic <abdoronic@gmail.com>
2020-12-28 05:09:18 +0200

    Add maximizing rooms area to obj. functoin
    Note: the satisfaction of each constraint will be represeanted as boolean value, as this seemed to improve performance (from non terminating to a terminating one).

---
+++ planner/constrainers/objective_function.py
@@ -0,0 +1,17 @@
+from planner.constrainers.utils import base_reify
+
+
+def add_objective_function(floor, model):
+    floor_area = floor.width * floor.length
+    rooms_area = [room.area_variable for room in floor.rooms]
+
+    floor_area_maximized = base_reify(
+        sum(rooms_area) == floor_area,
+        sum(rooms_area) != floor_area,
+        model
+    )
+
+    model.Add(floor.score_variable == sum([floor_area_maximized]))
+
+    model.Maximize(floor.score_variable)
+
